a6432a26285c97596195628a8da6f3fbf5b03055
marcop0 <pintomarco@gmx.com>
2017-02-20 20:05:44 +0100

    windows.bat

---
+++ windows.bat
@@ -0,0 +1,14 @@
+set folder=dashboard
+set publicassets=public
+rd .\release\%folder%\ /S /Q 
+mkdir release\%folder%
+mkdir release\%folder%\%publicassets%
+xcopy app.js release\%folder%\
+xcopy index.html release\%folder%\
+xcopy /E %publicassets% release\%folder%\%publicassets%\
+rd .\release\%folder%\%publicassets%\assets\compiled-js\ /S /Q 
+mkdir release\%folder%\%publicassets%\assets\compiled-js
+cmd /C browserify .\%publicassets%\assets\js\init.js -o .\release\%folder%\%publicassets%\assets\compiled-js\bundle.js
+cmd /C minify --output .\release\%folder%\%publicassets%\assets\compiled-js\bundle.min.js .\release\%folder%\%publicassets%\assets\compiled-js\bundle.js
+echo "Server Running"
+node ./release/dashboard/app.js
