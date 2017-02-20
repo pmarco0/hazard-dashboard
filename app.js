6dce94f36071364d7aed5d4505289a1299844a5c
marcop0 <pintomarco@gmx.com>
2017-02-20 20:33:00 +0100

    app.js comments

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+
+/*
+	Richiesta POST, incrementa il contatore di :value
+	e.g. POST 127.0.0.1:3000/cont/50
+*/
 app.post('/cont/:value', function (req,res){
 	var value = req.params.value;
 	if(isNaN(value)){
@@ -23,18 +28,33 @@ app.post('/cont/:value', function (req,res){
 	}
 });
 
+/*
+	Richiesta POST, incrementa il livello contagiosità
+	e.g. POST 127.0.0.1:3000/level/increase
+*/
 app.post('/level/increase', function(req,res){
 	console.log("Increase Level Request");
 	io.emit('increaseLevel');
 	res.status(200).send();
 });
 
+/*
+	Richiesta POST, decrementa il livello contagiosità
+	e.g. POST 127.0.0.1:3000/level/decrease
+*/
 app.post('/level/decrease', function(req,res){
 	console.log("Decrease Level Request");
 	io.emit('decreaseLevel');
 	res.status(200).send();
 });
 
+
+/*
+	Richiesta POST, scrive nel log
+	:type => {'danger','info','warning'}
+	:message => Messaggio da scrivere
+	e.g. POST 127.0.0.1:3000/danger/Attenzione Messaggio di Errore
+*/
 app.post('/chat/:type/:message', function(req,res){
 	var type = req.params.type;
 	var message = req.params.message;
@@ -42,6 +62,12 @@ app.post('/chat/:type/:message', function(req,res){
 	res.status(200).send();
 });
 
+
+/*
+	Richiesta POST, avvia o conclude il gioco
+	:type => 'start' per avviare, 'over' per concludere
+	e.g. POST 127.0.0.1:3000/game/start
+*/
 app.post('/game/:type', function(req,res){
 	var type = req.params.type;
 	switch(type){
