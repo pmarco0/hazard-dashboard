9ca22510c856d62655c8d8c593d63fd714748701
marcop0 <pintomarco@gmx.com>
2017-02-21 16:23:21 +0100

    Hazard Map

--- public/assets/vmap/setup.js
+++ public/assets/vmap/setup.js
@@ -6,23 +6,144 @@ var defaultFactor = -0.15; //Curvatura linea di collegamento [- verso l'alto, + 
 
 $(".map-container").mapael({
 		map: {
-			name: "world_countries_miller",
+			name: "hazard_map",
 			defaultArea : {
 				attrs: {
-					stroke: "#fff",
-					"stroke-width": 0.5
+					stroke: "#7C7C7C",
+					"stroke-width": 0.2
 				}
 			}
 		},
 	//QUI DEFINISCO I COLORI DELLE AREE
 	areas : {
-		"US": {
+		"Asia1": {
 			attrs : {
-				 fill: "#C01D2E"
+				 fill: "#FFFF00"
 			},
 		},
+		"Asia2": {
+			attrs : {
+				 fill: "#E4D82C"
+			},
+		},
+		"Asia3": {
+			attrs : {
+				 fill: "#DFD055"
+			},
+		},		
+		"Asia4": {
+			attrs : {
+				 fill: "#FCF463"
+			},
+		},		
+		"Asia5": {
+			attrs : {
+				 fill: "#F6F62F"
+			},
+		},		
+		"Asia6": {
+			attrs : {
+				 fill: "#FCC95F"
+			},
+		},		
+		"Asia7": {
+			attrs : {
+				 fill: "#909300"
+			},
+		},		
+		"Oce1": { //Australia
+			attrs : {
+				 fill: "#69B628"
+			},
+		},		
+		"Oce2": { //Nuova Zelanda
+			attrs : {
+				 fill: "#9DDE67"
+			},
+		},		
+		"Africa1": { //Nord Africa
+			attrs : {
+				 fill: "#444444"
+			},
+		},
+		"Africa2": { //Africa Centrale
+			attrs : {
+				 fill: "#000"
+			},
+		},		
+		"Africa3": { //Sud Centrale
+			attrs : {
+				 fill: "#828282"
+			},
+		},
+		"Africa4": { //Madagascar
+			attrs : {
+				 fill: "#CCCCCC"
+			},
+		},		
+		"Eu1": { //Eu Ovest
+			attrs : {
+				 fill: "#6699FF"
+			},
+		},		
+		"Eu2": { //Eu Est
+			attrs : {
+				 fill: "#00C0F8"
+			},
+		},		
+		"Eu3": { //Scandinavia
+			attrs : {
+				 fill: "#000080"
+			},
+		},		
+		"Eu4": { //UK
+			attrs : {
+				 fill: "#0074E4"
+			},
+		},		
+		"Eu5": { //Islanda
+			attrs : {
+				 fill: "#54D8FC"
+			},
+		},		
+		"America1": { //Groenlandia
+			attrs : {
+				 fill: "#FC0044"
+			},
+		},		
+		"America2": { //Canada
+			attrs : {
+				 fill: "#900808"
+			},
+		},		
+		"America3": { //USA
+			attrs : {
+				 fill: "#FF0000"
+			},
+		},		
+		"America4": { //America Centrale
+			attrs : {
+				 fill: "#CC0000"
+			},
+		},		
+		"America5": { //Sud America Colombia,Bolive ecc
+			attrs : {
+				 fill: "#E2004F"
+			},
+		},		
+		"America6": { //Sud America Brasile
+			attrs : {
+				 fill: "#FF525F"
+			},
+		},		
+		"America7": { //Sud America Argentina,Uruguay, ...
+			attrs : {
+				 fill: "#C61800"
+			},
+		},
+
 	},
-	
+	/*
 	//QUI DEFINISCO LE CITTA'
 	plots :{
 		'paris': {
@@ -51,5 +172,5 @@ $(".map-container").mapael({
 				'stroke-width': defaultStroke
 			}
 		}
-	}
+	}*/
 });
